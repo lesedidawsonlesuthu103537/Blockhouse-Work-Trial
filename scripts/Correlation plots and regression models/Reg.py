a3adcdafb3339fa9a59979c19254933390863994
lesedidawsonlesuthu103537 <lesuthulesedi@gmail.com>
2025-01-10 10:18:01 +0200

    Update Reg.py

--- scripts/Correlation plots and regression models/Reg.py
+++ scripts/Correlation plots and regression models/Reg.py
@@ -6,9 +6,10 @@ times3 = pd.DataFrame(pd.read_csv("PFE_times.csv"))
 times4 = pd.DataFrame(pd.read_csv("TSLA_times.csv"))
 times5 = pd.DataFrame(pd.read_csv("XOM_times.csv"))
 
+# This code chunk deals with finding the set of times common across the companies being investigated so that when running the regression models,
+# the data points are grouped in the correct time intervals.
+# This code chunk also orders the times in ascending order
 times=list(set(times1.iloc[:,1]) & set(times2.iloc[:,1]) & set(times3.iloc[:,1]) & set(times4.iloc[:,1]) & set(times5.iloc[:,1]))
-#print(pd.DataFrame(times))
-#print(len(times))
 l=len(times)
 timestamp=[]
 from datetime import time
@@ -22,16 +23,15 @@ for j in range(0,l):
     second=int(time_vec[2])
     timestamp.append(time(hour,minute,second))
 timestamp.sort()
-#print(timestamp)
 times=[]
 for j in range (0,l):
     tim=timestamp[j]
     tim.strftime("%H:%M:%S")
     dat_tim=vec[0]+"T"+str(tim)
     times.append(dat_tim)
-#print(pd.DataFrame(times))
 
-# Organising the AAPL data
+# Organising the AAPL data so that the data is arranged in the correct time intervals
+# This also creates a dataframe for the forecasted returns so that it is much easier to run the regression models
 data1=pd.DataFrame(pd.read_csv("AAPL_OFI.csv")) # OFIs
 data1_1=pd.DataFrame(pd.read_csv("AAPL_INT_OFI2.csv")) # Integrated OFIs
 data1_2=pd.DataFrame(pd.read_csv("AAPL_returns.csv")) # Current returns
@@ -39,8 +39,7 @@ ncol=data1.shape[1]
 data1=data1.iloc[:,1:ncol]
 data1_1=data1_1.iloc[:,1:ncol]
 data1_2=data1_2.iloc[:,1]
-#print(data1.head())
-#print(data1.iloc[:,0])
+
 import numpy as np
 dat1=np.zeros((10,l))
 dat1_1=np.zeros((1,l))
@@ -61,14 +60,10 @@ for j in range(0,l):
         dat1_1[0,j]=data1_1.iloc[0,index]
         dat1_2[j,0]=data1_2.iloc[index]
 
-#print(pd.DataFrame(dat1_1))
-#print(pd.DataFrame(dat1_2))
-#print(pd.DataFrame(dat1_3))
-#tt=pd.DataFrame(times)
-#tt.to_csv("Times.csv")
 
 
-# Organising the JPM data
+# Organising the JPM data so that the data is arranged in the correct time intervals
+# This also creates a dataframe for the forecasted returns so that it is much easier to run the regression models
 data2=pd.DataFrame(pd.read_csv("JPM_OFI.csv")) # OFIs
 data2_1=pd.DataFrame(pd.read_csv("JPM_INT_OFI2.csv")) # Integrated OFIs
 data2_2=pd.DataFrame(pd.read_csv("JPM_returns.csv")) # Current returns
@@ -84,6 +79,8 @@ dat2_1=np.zeros((1,l))
 dat2_2=np.zeros((l,1))
 dat2_3=np.zeros((l,1))
 for j in range(0,l):
+    # This if statement here because we cannot forecast the one minute returns for the last time interval because we would need price data from the
+    # following day, which is not included in our dataset
     if j<=l-2:
         all_times=list(times2.iloc[:,1])
         index =  all_times.index(times[j])
@@ -98,15 +95,9 @@ for j in range(0,l):
         dat2_1[0,j]=data2_1.iloc[0,index]
         dat2_2[j,0]=data2_2.iloc[index]
 
-#print(pd.DataFrame(dat2))
-#print(pd.DataFrame(dat2_1))
-#print(pd.DataFrame(dat2_2))
-#print(pd.DataFrame(dat2_3))
-#tt=pd.DataFrame(times)
-#tt.to_csv("Times.csv")
 
-
-# Organising the PFE data
+# Organising the PFE data so that the data is arranged in the correct time intervals
+# This also creates a dataframe for the forecasted returns so that it is much easier to run the regression models
 data3=pd.DataFrame(pd.read_csv("PFE_OFI.csv")) # OFIs
 data3_1=pd.DataFrame(pd.read_csv("PFE_INT_OFI2.csv")) # Integrated OFIs
 data3_2=pd.DataFrame(pd.read_csv("PFE_returns.csv")) # Current returns
@@ -122,6 +113,8 @@ dat3_1=np.zeros((1,l))
 dat3_2=np.zeros((l,1))
 dat3_3=np.zeros((l,1))
 for j in range(0,l):
+    # This if statement here because we cannot forecast the one minute returns for the last time interval because we would need price data from the
+    # following day, which is not included in our dataset
     if j<=l-2:
         all_times=list(times3.iloc[:,1])
         index =  all_times.index(times[j])
@@ -136,15 +129,9 @@ for j in range(0,l):
         dat3_1[0,j]=data3_1.iloc[0,index]
         dat3_2[j,0]=data3_2.iloc[index]
 
-#print(pd.DataFrame(dat3))
-#print(pd.DataFrame(dat3_1))
-#print(pd.DataFrame(dat3_2))
-#print(pd.DataFrame(dat3_3))
-#tt=pd.DataFrame(times)
-#tt.to_csv("Times.csv")
 
-
-# Organising the TSLA data
+# Organising the TSLA data so that the data is arranged in the correct time intervals
+# This also creates a dataframe for the forecasted returns so that it is much easier to run the regression models
 data4=pd.DataFrame(pd.read_csv("TSLA_OFI.csv")) # OFIs
 data4_1=pd.DataFrame(pd.read_csv("TSLA_INT_OFI2.csv")) # Integrated OFIs
 data4_2=pd.DataFrame(pd.read_csv("TSLA_returns.csv")) # Current returns
@@ -158,6 +145,8 @@ dat4_1=np.zeros((1,l))
 dat4_2=np.zeros((l,1))
 dat4_3=np.zeros((l,1))
 for j in range(0,l):
+    # This if statement here because we cannot forecast the one minute returns for the last time interval because we would need price data from the
+    # following day, which is not included in our dataset
     if j<=l-2:
         all_times=list(times4.iloc[:,1])
         index =  all_times.index(times[j])
@@ -172,13 +161,8 @@ for j in range(0,l):
         dat4_1[0,j]=data4_1.iloc[0,index]
         dat4_2[j,0]=data4_2.iloc[index]
 
-#print(pd.DataFrame(dat4_1))
-#print(pd.DataFrame(dat4_2))
-#print(pd.DataFrame(dat4_3))
-#tt=pd.DataFrame(times)
-#tt.to_csv("Times.csv")
-
-# Organising the XOM data
+# Organising the XOM data so that the data is arranged in the correct time intervals
+# This also creates a dataframe for the forecasted returns so that it is much easier to run the regression models
 data5=pd.DataFrame(pd.read_csv("XOM_OFI.csv")) # OFIs
 data5_1=pd.DataFrame(pd.read_csv("XOM_INT_OFI2.csv")) # Integrated OFIs
 data5_2=pd.DataFrame(pd.read_csv("XOM_returns.csv")) # Current returns
@@ -194,6 +178,8 @@ dat5_1=np.zeros((1,l))
 dat5_2=np.zeros((l,1))
 dat5_3=np.zeros((l,1))
 for j in range(0,l):
+    # This if statement here because we cannot forecast the one minute returns for the last time interval because we would need price data from the
+    # following day, which is not included in our dataset
     if j<=l-2:
         all_times=list(times5.iloc[:,1])
         index =  all_times.index(times[j])
@@ -208,30 +194,33 @@ for j in range(0,l):
         dat5_1[0,j]=data5_1.iloc[0,index]
         dat5_2[j,0]=data5_2.iloc[index]
 
-#print(pd.DataFrame(dat5))
-##print(pd.DataFrame(dat5_1))
-#print(pd.DataFrame(dat5_2))
-#print(pd.DataFrame(dat5_3))
-#tt=pd.DataFrame(times)
-#tt.to_csv("Times.csv")
 
-# Creating the design matrix
+# Creating the design matrix and the response vectors (one for contemporaneous returns and the other for forecasted returns)
 X=np.stack((dat1[0,:],dat2[0,:],dat3[0,:],dat4[0,:],dat5[0,:]),axis=0)
 X=np.transpose(X)
 X=pd.DataFrame(X)
 ret=np.concatenate((dat1_2,dat2_2,dat3_2,dat4_2,dat5_2),axis=1)
-ret2=np.concatenate((dat1_3,dat2_3,dat3_3,dat4_3,dat5_3),axis=1)
+ret2=np.concatenate((dat1_3,dat2_3,dat3_3,dat4_3,dat5_3),axis=1) # These are the forecasted returns
 X.columns=["AAPL","JPM","PFE","TSLA","XOM"]
 ret=pd.DataFrame(ret)
-ret2=pd.DataFrame(ret2)
+ret2=pd.DataFrame(ret2) # These are the forecasted returns
 ret.columns=["AAPL","JPM","PFE","TSLA","XOM"]
 ret2.columns=["AAPL","JPM","PFE","TSLA","XOM"]
 
 import statsmodels.api as sm
 
 print("Best-level OFI")
-#Creating the design matrix
+
+# Adding a constant to the design matrix
 X=sm.add_constant(X)
+
+
+# This section of the code runs the regression models for the best-level OFIs and integrated OFIs. It does so by regression the returns 
+# of each stock against the OFI of that stock (self-impact) and the OFIs of other stock (cross impact). Then it stores the regression 
+# coefficients of the self and cross impacts in matrices and stores the adjusted R^{2} in a vector. To summarise the impacts across all
+# the models, the mean and standard deviation of the self-impact and adjusted R^{2} vectors and cross-impact matrices are calculated,
+# which are then used in the report
+
 Self=np.zeros((5,1))
 R2=np.zeros((5,1))
 Cross=np.zeros((5,5))
@@ -243,10 +232,8 @@ for j in range(0,5):
     model = sm.OLS(Y, X).fit()
     Self[j]=model.params.iloc[j+1]
     R2[j]=model.rsquared_adj
-    Cross[j,:]=model.params.iloc[np.array(model.params)!=Self[j]]     
-    #print(model.summary())    
-#print(X.head())
-#print(ret.shape)
+    Cross[j,:]=model.params.iloc[np.array(model.params)!=Self[j]]       
+
 print("     ","Mean","Standard deviation",sep=" ")
 print("Self:",np.mean(Self),np.std(Self),sep=" ")
 print("R2:",np.mean(R2*100),np.std(R2*100),sep=" ")
@@ -273,6 +260,8 @@ print()
 print()
 
 
+# The procedure is repeated for the integrated OFI models
+
 print("Integrated OFI")
 # Creating the design matrix
 X2=np.concatenate((dat1_1,dat2_1,dat3_1,dat4_1,dat5_1),axis=0)
@@ -292,7 +281,6 @@ for j in range(0,5):
     Self[j]=model.params.iloc[j+1]
     R2[j]=model.rsquared_adj
     Cross[j,:]=model.params.iloc[np.array(model.params)!=Self[j]]    
-    #print(model.summary())    
 print("     ","Mean","Standard deviation",sep=" ")
 print("Self:",np.mean(Self),np.std(Self),sep=" ")
 print("R2:",np.mean(R2*100),np.std(R2*100),sep=" ")
@@ -307,7 +295,6 @@ Cross=np.zeros((5,5))
 for j in range(0,5):
     Y=ret2.iloc[:,j]
     model = sm.OLS(Y, X2).fit()
-    #print(model.summary())
     Self[j]=model.params.iloc[j+1]
     R2[j]=model.rsquared_adj
     Cross[j,:]=model.params.iloc[np.array(model.params)!=Self[j]]
